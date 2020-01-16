04c582ed370d1aa715e96bc27e30a3b823ea982d
Rahul255 <rahulmohan255@gmail.com>
2020-01-16 12:27:14 +0530

    Initial commit

---
+++ App/local_settings.py
@@ -0,0 +1,23 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0ld#g3eq90i4c$_mo27u%=#zhppr-q%n6liv5=6*19=3bh#0px'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
