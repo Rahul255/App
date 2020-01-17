9aecc5b1922136ea888251c9e955892800c2a945
Rahul255 <rahulmohan255@gmail.com>
2020-01-17 10:17:34 +0530

    file added

--- payment/admin.py
+++ payment/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from payment.inlines import OPInline
 from payment.models import Order, OrderProduct
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('name', 'orderID', 'amount')
+    inlines = (OPInline,)
 
 
 @admin.register(OrderProduct)
