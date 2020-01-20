079ef68e684438ac1488bb5f0366dc40ca91a710
Rahul255 <rahulmohan255@gmail.com>
2020-01-20 11:22:23 +0530

    file added

--- payment/models.py
+++ payment/models.py
@@ -15,13 +15,23 @@ from products.models import Pooja
 class Order(models.Model):
     name = models.CharField(max_length=150, null=True, blank=True)
     orderID = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    issuer = models.ForeignKey(User, on_delete=models.PROTECT)
+    issuer = models.CharField(max_length=150, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now=True)
     amount = models.PositiveIntegerField()
     products = models.ManyToManyField(Pooja, through='OrderProduct')
+    total = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    lastEditTime = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
+
+    def save_model(self, request, obj, form, change):
+        obj.total = request.user
+        obj.lastEditTime = datetime.now()
+        for product in OrderProduct.objects.filter(order=Order):
+            cost = cost + product.price * product.qty
+            obj.total = cost
+        super().save_model(request, obj, form, change)
 
 
 class OrderProduct(models.Model):
@@ -30,7 +40,6 @@ class OrderProduct(models.Model):
     price = models.PositiveIntegerField()
     date = models.DateTimeField(null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    total = models.IntegerField()
 
     class Meta:
         verbose_name_plural = "Order Products"
@@ -38,20 +47,3 @@ class OrderProduct(models.Model):
 
     def __str__(self):
         return str(self.pooja.pooja + ' - ' + self.order.name)
-
-    def save_model(self, request, obj, form, change):
-        obj.lastEditor = request.user
-        obj.lastEditTime = datetime.now()
-        super().save_model(request, obj, form, change)
-        for product in OrderProduct.objects.filter(order=order):
-            cost = cost + product.price * product.qty
-            obj.total = cost
-
-    class Competition(models.Model):
-        lastEditor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-        lastEditTime = models.DateTimeField(null=True, blank=True)
-
-
-
-
-
